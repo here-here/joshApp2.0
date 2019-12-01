ccfc7177222c19ad5b86d6e2ecd8f4a037a9bd95
stevenlittle-UCF <54563826+stevenlittle-UCF@users.noreply.github.com>
2019-12-01 18:58:32 -0500

    Bluetooth now works
    Need to add API call

--- lib/checkin/checkin_form.dart
+++ lib/checkin/checkin_form.dart
@@ -6,30 +6,143 @@ import 'package:flutter_login/checkin/checkin.dart';
 
 import 'package:flutter_beacon/flutter_beacon.dart';
 import 'package:device_info/device_info.dart';
+import 'dart:async';
 import 'package:flutter/services.dart';
 import 'dart:io';
-import 'dart:async';
+
 
 class CheckinForm extends StatefulWidget {
-  final String uuid;
-
-  CheckinForm({Key key, this.uuid}) : super (key: key);
-
   @override
   State<CheckinForm> createState() => _checkinFormState();
 }
 
-class _checkinFormState extends State<CheckinForm> with WidgetsBindingObserver {
+class _checkinFormState extends State<CheckinForm> {
   final _pidController = TextEditingController();
   final _classController = TextEditingController();
-  String id;
 
-  String searchUUID = '39ed98ff-2900-441a-802f-9c398fc199d2';
+  @override
+  Widget build(BuildContext context) {
+//    _onCheckinButtonPressed() {
+//      BlocProvider.of<CheckinBloc>(context).add(
+//        CheckinButtonPressed(
+//          pid: _pidController.text,
+//          class_name: _classController.text,
+//        ),
+//      );
+//    }
+
+    return BlocListener<CheckinBloc, CheckinState>(
+      listener: (context, state) {
+        if (state is CheckinFailure) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text('${state.error}'),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      },
+      child: BlocBuilder<CheckinBloc, CheckinState>(
+        builder: (context, state) {
+          return Form(
+            child:
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                  child:
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      labelText: "Enter PID",
+                      fillColor: Colors.white,
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                        borderSide: new BorderSide(
+                        ),
+                      ),
+                      //fillColor: Colors.green
+                    ),
+                    controller: _pidController,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 0.0),
+                  child:
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      labelText: "Enter Class ID",
+                      fillColor: Colors.white,
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                        borderSide: new BorderSide(
+                        ),
+                      ),
+                      //fillColor: Colors.green
+                    ),
+                    controller: _classController,
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () async {
+
+                    // Check if text was entered
+                    if (_pidController.text != null && _classController.text != null) {
+                      /// navigate to search screen
+                      var route = new MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                        new MyApp(uuid: '39ed98ff-2900-441a-802f-9c398fc199d2',
+                            pid: _pidController.text,
+                            classCode: _classController.text),
+                      );
+                      Navigator.push(context, route);
+                    }
+                    else {
+                      return showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text("Please enter the values for both fields!"),
+                          );
+                        },
+                      );
+                    }
+                    },
+
+                  child: Text('checkin'),
+                ),
+
+                Container(
+                  child: state is CheckinLoading
+                      ? CircularProgressIndicator()
+                      : null,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  final String uuid;
+  final String pid;
+  final String classCode;
+
+  MyApp({Key key, this.uuid, this.pid, this.classCode}) : super (key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  String id;
+  String searchUUID;
   final String region = "com.attendhere.beacon";
   final StreamController<BluetoothState> streamController = StreamController();
   StreamSubscription<BluetoothState> _streamBluetooth;
   StreamSubscription<RangingResult> _streamRanging;
-
 //  final _regionBeacons = <Region, List<Beacon>>{};
   final _beacons = <Beacon>[];
   bool authorizationStatusOk = false;
@@ -39,22 +152,22 @@ class _checkinFormState extends State<CheckinForm> with WidgetsBindingObserver {
 
 //  BeaconBroadcast beaconBroadcast = BeaconBroadcast();
 
+  @override
   void initState() {
+    if (widget.uuid != null) {
+      searchUUID = widget.uuid;
+    }
     WidgetsBinding.instance.addObserver(this);
 
     super.initState();
 
-//    searchUUID = widget.uuid;
-
-    return listeningState();
+    listeningState();
   }
 
   listeningState() async {
     print("starting");
-//    print("${widget.UUID}");
     id = await makeDeviceID();
     print(id);
-//    Future<bool> result;
     print('Listening to bluetooth state');
     _streamBluetooth = flutterBeacon
         .bluetoothStateChanged()
@@ -72,7 +185,6 @@ class _checkinFormState extends State<CheckinForm> with WidgetsBindingObserver {
           break;
       }
     });
-//    return result;
   }
 
   checkAllRequirements() async {
@@ -132,15 +244,12 @@ class _checkinFormState extends State<CheckinForm> with WidgetsBindingObserver {
                 // stop searching
                 pauseScanBeacon();
 
+                // Go to next screen with device id to contact database
                 Navigator.pop(context);
-
-                /// This is where we hit the api to register then navigate to the next screen
               }
             }
           }
-//          return false;
         });
-//    return false;
   }
 
   pauseScanBeacon() async {
@@ -151,7 +260,6 @@ class _checkinFormState extends State<CheckinForm> with WidgetsBindingObserver {
       });
     }
   }
-
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) async {
@@ -174,13 +282,51 @@ class _checkinFormState extends State<CheckinForm> with WidgetsBindingObserver {
 
   @override
   void dispose() {
-//    WidgetsBinding.instance.removeObserver(this);
+    WidgetsBinding.instance.removeObserver(this);
     streamController?.close();
     _streamRanging?.cancel();
     _streamBluetooth?.cancel();
     flutterBeacon.close;
 
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/knighttro.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  RaisedButton(
+                    color: Colors.red,
+                    child: Text("Search for Beacons!"),
+                    onPressed: () async {
+                      await flutterBeacon.requestAuthorization;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )
+
+      ),
+    );
   }
 
   static Future<List<String>> getDeviceDetails() async {
@@ -221,89 +367,5 @@ class _checkinFormState extends State<CheckinForm> with WidgetsBindingObserver {
     }
 
     return retVal;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-//    _onCheckinButtonPressed() {
-//      BlocProvider.of<CheckinBloc>(context).add(
-//        CheckinButtonPressed(
-//          pid: _pidController.text,
-//          class_name: _classController.text,
-//        ),
-//      );
-//    }
-
-    return BlocListener<CheckinBloc, CheckinState>(
-      listener: (context, state) {
-        if (state is CheckinFailure) {
-          Scaffold.of(context).showSnackBar(
-            SnackBar(
-              content: Text('${state.error}'),
-              backgroundColor: Colors.red,
-            ),
-          );
-        }
-      },
-      child: BlocBuilder<CheckinBloc, CheckinState>(
-        builder: (context, state) {
-          return Form(
-            child:
-              Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                    child:
-                    TextFormField(
-                      decoration: new InputDecoration(
-                        labelText: "Enter PID",
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(25.0),
-                          borderSide: new BorderSide(
-                          ),
-                        ),
-                        //fillColor: Colors.green
-                      ),
-                      controller: _pidController,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 0.0),
-                    child:
-                      TextFormField(
-                        decoration: new InputDecoration(
-                        labelText: "Enter Class ID",
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                        borderRadius: new BorderRadius.circular(25.0),
-                        borderSide: new BorderSide(
-                        ),
-                        ),
-                        //fillColor: Colors.green
-                        ),
-                      controller: _classController,
-                      ),
-                    ),
-                  RaisedButton(
-                    onPressed: () async {
-                      initState();
-//                      await flutterBeacon.requestAuthorization;
-//                      state is! CheckinLoading ? _onCheckinButtonPressed : null,
-                    },
-                    child: Text('checkin'),
-                  ),
-
-                  Container(
-                    child: state is CheckinLoading
-                        ? CircularProgressIndicator()
-                        : null,
-                  ),
-                ],
-              ),
-          );
-        },
-      ),
-    );
   }
 }
